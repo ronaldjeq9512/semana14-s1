38d19716cde3c701e0304c6f807a6fef4b569f90
ronaldjeq9512 <ronaldjhairestelaquintana@gmail.com>
2024-06-17 22:47:01 -0500

    configurate vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/semana14-s1/',  // Reemplaza 'NOMBRE_DEL_REPOSITORIO' con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+  }
+})
